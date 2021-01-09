af27de7f880a02efbad79e705f61514c3f8248df
SUPERADEGA\Vinicius.ti <viniciuz.lol5@gmail.com>
2021-01-09 19:54:13 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,58 @@
+let modalQT = 1;
+
+const c = (el)=> document.querySelector(el);
+
+const cs = (el)=> document.querySelectorAll(el);
+
+
+
+//LISTAGEM DAS PIZZAS
+pizzaJson.map((item,index)=>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+    //PREENCHER AS INFORMAÇÕES EM PIZZA ITEM;
+
+    pizzaItem.setAttribute('data-key', index);
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('a').addEventListener('click',(event)=>{
+        event.preventDefault();
+        let key = event.target.closest('.pizza-item').getAttribute('data-key');
+        modalQT = 1;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+
+        cs('.pizzaInfo--size').forEach((size,sizeIndex)=>{
+            if(sizeIndex == 2) {
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+        c('.pizzaInfo--qt').innerHTML = modalQT;
+
+
+
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = 1;
+        },100);
+    });    
+    c('.pizza-area').append(pizzaItem);
+});
+
+//EVENTOS DO MODAL 
+
+
+
+
+
+
+
+
+
+
